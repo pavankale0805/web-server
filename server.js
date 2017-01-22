679d6b909ef54f268f4bca90a97b84eafb285bbc
Pavan Kale <pavankale1234@gmail.com>
2017-01-22 10:17:34 +0530

    Changes when video viewed second time

--- server.js
+++ server.js
@@ -15,3 +15,28 @@ app.use(express.static(__dirname + '/public'));
 app.listen(PORT, function() {
 	console.log('Express server started on port ' + PORT + '!');
 });
+
+
+
+/*var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('Private route hit!');
+		next();
+	}
+}
+
+app.use(middleware.requireAuthentication);
+
+app.get('/', function (req, res) {
+	res.send('Hellow Express!');
+});
+
+app.get('/about', function (req, res) {
+	res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() {
+	console.log('Express server started on port ' + PORT + '!');
+});*/
